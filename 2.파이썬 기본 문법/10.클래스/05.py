f08c32eb4252e7e74c0099d48084113d8b1c8b2e
kyuyounglee-20250607 <pia222.20250607@gmail.com>
2025-09-21 15:06:40 +0900

    .

--- 2.파이썬 기본 문법/10.클래스/05.py
+++ 2.파이썬 기본 문법/10.클래스/05.py
@@ -1,20 +1,16 @@
 
-# 파이썬 내장 DB 
-class Customer:
-    def __init__(self,name,email,age = 18):
-        self.name = name
-        self.email = email
-        self.age = age
-        self.loyalty_point = 0  # 포인트
-    def earn_points(self,amount):
-        self.loyalty_point += amount
-        return f'{self.name}님의 현재 포인트는 {self.loyalty_point}입니다.'
-    def get_prifile(self):
-        return f'프로필:{self.name}({self.age}세), 이메일:{self.email}, \
-        포인트:{self.loyalty_point}'
-
-# 인스턴스 생성
-c1 = Customer('홍길동','hong@gmail.com',25)
-# 메소드 사용
-print(c1.earn_points(200))
-print(c1.get_prifile())
+# 파이썬 내장 DB   name email age loyalty_point
+import sqlite3
+# db 연결 : 파일 'test.db' 생성
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+# 테이블 생성
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        age INTEGER DEFAULT 18,
+        loyalty_points INTEGER DEFAULT 0
+    )
+''')

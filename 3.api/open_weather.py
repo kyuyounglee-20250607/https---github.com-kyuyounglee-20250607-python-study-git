e52152c470a99a33b7cf54789f5309703cd8733e
kyuyounglee-20250607 <pia222.20250607@gmail.com>
2025-09-27 12:42:57 +0900

    .

--- 3.api/open_weather.py
+++ 3.api/open_weather.py
@@ -15,8 +15,9 @@ params = {
 response = requests.get(url, params=params)
 
 if response.status_code == 200:
+    import json
     data = response.json()
-    print(data)
+    print(json.dumps(data, indent=4,ensure_ascii=False))    
 else:
     print('Error:', response.status_code)
 
